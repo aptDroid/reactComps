7c64bef5677ca4457cc18911bd0e2c98c60f478e
aptDroid <apt.sharp@gmail.com>
2018-06-27 19:09:34 +0530

    profileLanding

---
+++ src/components/Landing/ProfileLanding.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./ProfileLanding.scss";
+
+export class ProfileLanding extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    let props = this.props;
+    return (
+      <div id="profileBrief">
+        <div id="skillLine">{props.skillLine}</div>
+        <br />
+        <div id="supportingLine">{props.supportingLine}</div>
+        <div id="profileFooter">
+          <label id="location">Based out of {props.location} |</label>
+          <button id="workButton">
+            See my work <span>&#10174;</span>
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -2,50 +2,83 @@ import React from "react";
 import { render } from "react-dom";
 import { Navbar } from "./components/Navbar/Navbar";
 import { Profile } from "./components/Profile/Profile";
+import { ProfileLanding } from "./components/Landing/ProfileLanding";
 
 import "./styles.scss";
 
-const App = () => (
-  <div>
-    <Navbar
-      list={[
-        {
-          name: "google",
-          href: "https://www.google.com/"
-        },
-        {
-          name: "facebook",
-          href: "https://www.facebook.com"
-        },
-        {
-          name: "deviantart",
-          href: "https://aptdroid.deviantart.com/gallery/"
-        }
-      ]}
-    />
-    <br />
-    <div id="pCont">
-      <Profile
-        img={
-          "https://vignette.wikia.nocookie.net/vsbattles/images/a/a7/Ancient-apparition-fanart-wallpaper.png/revision/latest?cb=20170919204549"
-        }
-        name={"Ancient Appartition"}
+const App = () => {
+  /*return (
+    <div>
+      <Navbar
+        list={[
+          {
+            name: "google",
+            href: "https://www.google.com/"
+          },
+          {
+            name: "facebook",
+            href: "https://www.facebook.com"
+          },
+          {
+            name: "deviantart",
+            href: "https://aptdroid.deviantart.com/gallery/"
+          }
+        ]}
       />
+      <br />
+      <div id="pCont">
+        <Profile
+          img={
+            "https://vignette.wikia.nocookie.net/vsbattles/images/a/a7/Ancient-apparition-fanart-wallpaper.png/revision/latest?cb=20170919204549"
+          }
+          name={"Ancient Appartition"}
+        />
 
-      <Profile
-        img={
-          "https://vignette.wikia.nocookie.net/vsbattles/images/a/a7/Ancient-apparition-fanart-wallpaper.png/revision/latest?cb=20170919204549"
-        }
-        name={"Ancient Appartition"}
-      />
-      <Profile
-        img={
-          "https://vignette.wikia.nocookie.net/vsbattles/images/a/a7/Ancient-apparition-fanart-wallpaper.png/revision/latest?cb=20170919204549"
-        }
-        name={"Ancient Appartition"}
-      />
+        <Profile
+          img={
+            "https://vignette.wikia.nocookie.net/vsbattles/images/a/a7/Ancient-apparition-fanart-wallpaper.png/revision/latest?cb=20170919204549"
+          }
+          name={"Ancient Appartition"}
+        />
+        <Profile
+          img={
+            "https://vignette.wikia.nocookie.net/vsbattles/images/a/a7/Ancient-apparition-fanart-wallpaper.png/revision/latest?cb=20170919204549"
+          }
+          name={"Ancient Appartition"}
+        />
+      </div>
     </div>
-  </div>
-);
-
+  );
+  */
+  return (
+    <React.Fragment>
+      <Navbar
+        list={[
+          {
+            name: "google",
+            href: "https://www.google.com/"
+          },
+          {
+            name: "facebook",
+            href: "https://www.facebook.com"
+          },
+          {
+            name: "deviantart",
+            href: "https://aptdroid.deviantart.com/gallery/"
+          }
+        ]}
+      />
+      <ProfileLanding
+        skillLine={"CARRY"}
+        supportingLine={"SUPP HERO"}
+        location={"Bangalore"}
+      />
+      <ProfileLanding
+        skillLine={"CARRY"}
+        supportingLine={"SUPP HERO"}
+        location={"Bangalore"}
+      />
+    </React.Fragment>
+  );
+};
 render(<App />, document.getElementById("app"));
